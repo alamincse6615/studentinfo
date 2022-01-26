9bbef5b982d578316b5d7d4d9a85308eedde36c9
Zihadul Islam <94509317+zihad650@users.noreply.github.com>
2022-01-26 11:48:51 +0600

    Update main.dart
    Zihad

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+// Mobile diya commit cheek korlam
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
